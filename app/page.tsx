81b7a77afc8e7b25fa79230118e63cc660aa0388
Yono1112 <yuma.ohno.sss@gmail.com>
2024-06-05 17:04:28 +0900

    install mui-icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import { Button, Fab, Stack, TextField, Typography } from "@mui/material";
+import  AddIcon from "@mui/icons-material/Add"
 
 export default function Home() {
   return (
     <Stack height="100lvh" justifyContent="center" alignItems="center" gap="32px">
       <Typography id="login_heading" variant="h1" fontSize="1.5rem">ログインフォーム</Typography>
       <Stack component="form" width={560} gap="24px" aria-labelledby="login_heading">
-        <TextField label="メールアドレス" />
+        <TextField label="メールアドレス" variant="filled" />
         <TextField label="パスワード" />
         <Button variant="contained">ログイン</Button>
       </Stack>
+      <Fab color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      <TextField
+          error
+          id="filled-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="filled"
+        />
     </Stack>
   );
 }
